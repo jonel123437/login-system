e9fb763259a0642dc108164fcd8a5d20de5bc355
jonel123437 <145882424+jonel123437@users.noreply.github.com>
2024-06-20 21:38:48 +0800

    .

--- View/login.php
+++ View/login.php
@@ -3,13 +3,14 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Login</title>
+        <link rel="stylesheet" href="assets/css/styles.css">
     </head>
     <body>
         <div class="container">
             <form action="?page=dashboard" method="POST">
                 <h1>Login</h1>
                 <div>
-                    <input type="submit" value="submit">
+                    <input type="submit" value="Login">
                 </div>
                 <div>Dont have an account yet? <a href="?page=register">Signup</a></div>
             </form>
--- View/register.php
+++ View/register.php
@@ -3,11 +3,15 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Register</title>
+        <link rel="stylesheet" href="assets/css/styles.css">
     </head>
     <body>
         <div class="container">
             <form action="">
                 <h1>Register</h1>
+                <div>
+                    <input type="submit" value="Signup">
+                </div>
                 <div>Already have an account? <a href="?page=login">Login</a></div>
             </form>
         </div>
